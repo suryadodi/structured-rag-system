895167ceb1fc1965093dd4d1177d30d1263fcaab
ksuryadodi <ksuryaj1892@gmail.com>
2026-04-23 01:48:33 +0530

    tablescript

--- app/ingestion/extractor.py
+++ app/ingestion/extractor.py
@@ -2,6 +2,8 @@ import fitz
 import pdfplumber
 from typing import List,Dict,Any,Optional
 import os
+from openai import OpenAI
+import base64
 from app.config.settings import app_settings
 
 class PDFExtractor:
@@ -31,6 +33,21 @@ class PDFExtractor:
                     })
         return table_data
 
+# summarize the table
+    
+    def table_summary(self, table_data:List[List[Any]])->str:
+        client=OpenAI(api_key=self.settings.OPENAI_API_KEY)
+        table_list = "\n".join([str(row) for row in table_data])
+        response=client.chat.completions.create(
+            model=self.settings.LLM_MODEL,
+            messages=[{
+                "role":"user",
+                "content":f"summarize the key information and trends in this table from a professional document in short sentences that suits for vector db from {table_list}"
+            }],
+            max_tokens=self.settings.MAX_TOKENS,
+        )
+        return response.choices[0].message.content
+
 # The Image or Graph Extractor
     
     def extract_graph(self, filepath:str):
@@ -48,6 +65,37 @@ class PDFExtractor:
                 })
         doc.close()
         return visuals
+
+
+# Image to text
+  
+    def image_text(self, image_bytes:bytes):
+        client = OpenAI(api_key=self.settings.OPENAI_API_KEY)
+        base64_image = base64.b64encode(image_bytes).decode("utf-8")
+        response = client.chat.completions.create(
+            model=self.settings.LLM_MODEL,
+            messages=[{
+                "role":"user",
+                "content":[
+                    {
+                        "type":"text",
+                        "text":"Describe this image or graph in detail from a professional document in detail.if it is a chart or graph explain the trends and data points."},
+                        {
+                         "type":"image_url",
+                         "image_url":{
+                            "url":f"data:image/png;base64,{base64_image}"
+                         }
+                         }
+                        
+                    
+                ]
+            }
+            ],
+            max_tokens = self.settings.MAX_TOKENS,
+            
+        )
+        return response.choices[0].message.content
+
 
 
 
