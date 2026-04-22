21817e3345b856d8a63c2e3720f92d825fa36838
ksuryadodi <ksuryaj1892@gmail.com>
2026-04-23 03:26:27 +0530

    Manager.py

---
+++ app/ingestion/manager.py
@@ -0,0 +1,54 @@
+import os
+from app.ingestion.registry import DocumentRegistry
+from app.ingestion.extractor import PDFExtractor
+from app.utils.logger import get_structured_logger
+
+logger = get_structured_logger(__name__)
+
+class IngestionManager:
+
+    def __init__(self):
+        self.registry = DocumentRegistry()
+        self.extractor = PDFExtractor()
+
+    def document_process(self,filepath:str):
+        file_hash = self.registry.generate_hash(filepath)
+        filename = os.path.basename(filepath)
+        if self.registry.is_processed(file_hash):
+            logger.info(f"{filename} already processed, skipping." ,extra={"step":"ingestion"})
+            return None
+        else:
+            logger.info(f"Starting ingestion for {filename}",extra={"step":"ingestion"})
+            text = self.extractor.extract_text(filepath)
+            raw_tables = self.extractor.extract_tables(filepath)
+            table_summaries =[]
+            for table in raw_tables:
+                summary = self.extractor.table_summary(table['raw'])
+                table_summaries.append({
+                    "content":summary,
+                    "metadata":{
+                        "type":"table",
+                        "page":table['page']
+                    }
+                })
+            images_data = self.extractor.extract_graph(filepath)
+            image_summaries =[]
+            for image in images_data:
+                image_summary =self.extractor.image_text(image["image_bytes"])
+                image_summaries.append({
+                    "content":image_summary,
+                    "metadata":{
+                        "type":"image",
+                        "page":image['page']
+                    }
+
+                })
+            self.registry.mark_processed(file_hash,filename)
+            logger.info(f"Successfully processed {filename}",extra={"step":"ingestion"})
+            return{
+            "text":text,
+            "tables":table_summaries,
+            "images":image_summaries
+        }
+    
+        
